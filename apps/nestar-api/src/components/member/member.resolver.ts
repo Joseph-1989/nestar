0a8168e3f6c4e9c267fe9e041d503eca373e1d9f
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-05-11 20:20:02 +0900

    feat: develop signup business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,33 +1,56 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member } from '../../libs/dto/member/member';
 
 @Resolver()
 export class MemberResolver {
 	constructor(private readonly memberService: MemberService) {}
-	@Mutation(() => String)
-	@UsePipes(ValidationPipe)
-	public async signup(@Args('input') input: MemberInput): Promise<string> {
-		console.log('Mutation: signup');
-		console.log('input:', input);
-		return this.memberService.signup();
-	}
-	@Mutation(() => String)
-	public async login(@Args('input') input: LoginInput): Promise<string> {
-		console.log('Mutation: login');
-		console.log('input:', input);
-		return this.memberService.login();
-	}
-	@Mutation(() => String)
-	public async updateMember(): Promise<string> {
-		console.log('Mutation: updateMember');
-		return this.memberService.updateMember();
-	}
 
-	@Query(() => String)
-	public async getMember(): Promise<string> {
-		console.log('Query: getMember');
-		return this.memberService.getMember();
+	@Mutation(() => Member)
+	@UsePipes(ValidationPipe)
+	public async signup(@Args('input') input: MemberInput): Promise<Member> {
+		try {
+			console.log('Mutation: signup');
+			console.log('input:', input);
+			return await this.memberService.signup(input);
+		} catch (err) {
+			console.log('ERR: signup', err);
+			throw new InternalServerErrorException(err);
+		}
 	}
+	@Mutation(() => Member)
+	public async login(@Args('input') input: LoginInput): Promise<Member> {
+		try {
+			console.log('Mutation: login');
+			console.log('input:', input);
+			return await this.memberService.login(input);
+		} catch (err) {
+			console.log('ERR: login', err);
+			throw new InternalServerErrorException(err);
+		}
+	}
+	// @Mutation(() => Member)
+	// public async updateMember(): Promise<Member> {
+	// 	try {
+	// 		console.log('Mutation: updateMember');
+	// 		// console.log('input:', input);
+	// 		return await this.memberService.updateMember();
+	// 	} catch (err) {
+	// 		console.log('ERR: updateMember', err);
+	// 		throw new InternalServerErrorException(err);
+	// 	}
+	// }
+
+	// @Query(() => Member)
+	// public async getMember(): Promise<Member> {
+	// 	try {
+	// 		console.log('Query: getMember');
+	// 		return await this.memberService.getMember(input);
+	// 	} catch (err) {
+	// 		console.log('ERR: getMember', err);
+	// 		throw new InternalServerErrorException(err);
+	// 	}
+	// }
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,23 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
-	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
-	public async signup(): Promise<string> {
-		return 'signup executed!';
+	constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
+	public async signup(input: MemberInput): Promise<Member> {
+		try {
+			const result = await this.memberModel.create(input);
+			return result;
+		} catch (err) {
+			console.log('Error: Service.model:', err);
+			throw new BadRequestException(err);
+		}
 	}
 
-	public async login(): Promise<string> {
-		return 'login executed!';
+	public async login(input: LoginInput): Promise<Member> {
+		try {
+			const result = await this.memberModel.create(input);
+			return result;
+		} catch (err) {
+			console.log('Error: Model.service:', err);
+			throw new InternalServerErrorException(err);
+		}
 	}
 
-	public async updateMember(): Promise<string> {
-		return 'updateMember executed!';
+	public async updateMember(input: MemberInput): Promise<Member> {
+		try {
+			const result = await this.memberModel.create(input);
+			return result;
+		} catch (err) {
+			console.log('Error: Model.service:', err);
+			throw new InternalServerErrorException(err);
+		}
 	}
 
-	public async getMember(): Promise<string> {
-		return 'getMember executed!';
+	public async getMember(input: MemberInput): Promise<Member> {
+		try {
+			const result = await this.memberModel.create(input);
+			return result;
+		} catch (err) {
+			console.log('Error: Model.service:', err);
+			throw new InternalServerErrorException(err);
+		}
 	}
 }
