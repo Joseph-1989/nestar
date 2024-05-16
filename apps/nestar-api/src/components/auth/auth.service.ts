fb5753f233165a46ad17a9e6549197349ac7b93d
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-05-16 09:04:53 +0900

    fix: creation and verification jwt

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
-
+import { JwtService } from '@nestjs/jwt';
+import { Member } from '../../libs/dto/member/member';
 @Injectable()
 export class AuthService {
+	constructor(private JwtService: JwtService) {}
 	public async hashPassword(memberPassword: string): Promise<string> {
 		const salt = await bcrypt.genSalt();
 		return await bcrypt.hash(memberPassword, salt);
@@ -10,5 +12,19 @@ export class AuthService {
 
 	public async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
 		return await bcrypt.compare(password, hashedPassword);
+	}
+
+	public async createToken(member: Member): Promise<string> {
+		const payload: T = {};
+		Object.keys(member['_doc'] ? member['_doc'] : member).map((ele) => {
+			payload[`${ele}`] = member[`${ele}`];
+		});
+		console.log('Memeber:', member);
+		return await this.JwtService.signAsync(payload);
+	}
+
+	public async verifyToken(token: string): Promise<T> {
+		const member = await this.JwtService.verifyAsync(token);
+		return member;
 	}
 }
