cff7a5481897d33f9efcc2aa67ae255da4a183d2
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-05-24 11:25:04 +0900

    feat: TASK-ZQ

--- train copy.ts
+++ train copy.ts
@@ -121,3 +121,30 @@ function areParenthesesBalanced(inputString: string): boolean {
 }
 
 console.log(areParenthesesBalanced('string()ichida(qavslar)soni()balansda (())'));
+
+// ZP-TASK:
+
+// Shunday function yozing, u parametridagi array ichida
+// eng kop takrorlangan raqamni topib qaytarsin.
+// MASALAN: majorityElement([1,2,3,4,5,4,3,4]) return 4
+
+function majorityElement(arr: number[]): number | undefined {
+	if (arr.length === 0) return undefined;
+
+	const elementCounts = new Map<number, number>();
+
+	for (const num of arr) {
+		const currentCount = elementCounts.get(num) || 0;
+		elementCounts.set(num, currentCount + 1);
+	}
+
+	const maxCount = Math.max(...elementCounts.values());
+
+	for (const [num, count] of elementCounts.entries()) {
+		if (count === maxCount) return num;
+	}
+
+	return undefined;
+}
+
+console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4, 8, 8, 8, 8, 999999])); // Output: 4
--- train.ts
+++ train.ts
@@ -1,26 +1,27 @@
-// ZP-TASK:
+// ZQ-TASK:
 
 // Shunday function yozing, u parametridagi array ichida
-// eng kop takrorlangan raqamni topib qaytarsin.
-// MASALAN: majorityElement([1,2,3,4,5,4,3,4]) return 4
-
-function majorityElement(arr: number[]): number | undefined {
-	if (arr.length === 0) return undefined;
-
-	const elementCounts = new Map<number, number>();
+// 2 marta qaytarilgan sonlarni alohida araryda qaytarsin.
+// MASALAN: findDuplicates([1,2,3,4,5,4,3,4]) return [3, 4]
+function findDuplicates(arr: number[]): number[] {
+	const countMap: { [key: number]: number } = {};
+	const duplicates: number[] = [];
 
 	for (const num of arr) {
-		const currentCount = elementCounts.get(num) || 0;
-		elementCounts.set(num, currentCount + 1);
+		if (countMap[num]) {
+			countMap[num]++;
+		} else {
+			countMap[num] = 1;
+		}
 	}
 
-	const maxCount = Math.max(...elementCounts.values());
-
-	for (const [num, count] of elementCounts.entries()) {
-		if (count === maxCount) return num;
+	for (const key in countMap) {
+		if (countMap[key] > 1) {
+			duplicates.push(Number(key));
+		}
 	}
 
-	return undefined;
+	return duplicates;
 }
 
-console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4, 8, 8, 8, 8, 999999])); // Output: 4
+console.log(findDuplicates([1, 2, 3, 10, 4, 5, 5, 5, 5, 4, 3, 4, 8, 8, 8, 8, 10, 999999]));
