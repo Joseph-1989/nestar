52bd4581e40af61c1d4694a08c38b90476737877
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-05-29 20:42:33 +0900

    feat: TASK-ZS

--- train copy.ts
+++ train copy.ts
@@ -176,3 +176,22 @@ function findDuplicates(arr: number[]): number[] {
 }
 
 console.log(findDuplicates([1, 2, 3, 10, 4, 5, 5, 5, 5, 4, 3, 4, 8, 8, 8, 8, 10, 999999]));
+
+// ZR-TASK:
+
+// Shunday function yozing, u parametridagi string ichidagi raqam va sonlarni sonini sanasin.
+// MASALAN: countNumberAndLetters(“string152%\¥”) return {number:3, letter:6}
+function countNumberAndLetters(str) {
+	let number = 0;
+	let letter = 0;
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] >= '0' && str[i] <= '9') {
+			number++;
+		} else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+			letter++;
+		}
+	}
+	return { number, letter };
+}
+console.log(countNumberAndLetters('string152%¥'));
+console.log(countNumberAndLetters('JOSEPH770077%¥'));
--- train.ts
+++ train.ts
@@ -1,18 +1,31 @@
-// ZR-TASK:
+// ZS-TASK:
 
-// Shunday function yozing, u parametridagi string ichidagi raqam va sonlarni sonini sanasin.
-// MASALAN: countNumberAndLetters(“string152%\¥”) return {number:3, letter:6}
-function countNumberAndLetters(str) {
-	let number = 0;
-	let letter = 0;
-	for (let i = 0; i < str.length; i++) {
-		if (str[i] >= '0' && str[i] <= '9') {
-			number++;
-		} else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
-			letter++;
+// Shunday function yozing, u parametridagi arrayni
+// ichidagi 1 marta kelgan elemnetni qaytarsin.
+// MASALAN: singleNumber([4, 2, 1, 2, 1]) return 4
+function singleNumber(nums) {
+	const map = new Map();
+
+	// Count each element in the array
+	for (const num of nums) {
+		if (map.has(num)) {
+			map.set(num, map.get(num) + 1);
+		} else {
+			map.set(num, 1);
 		}
 	}
-	return { number, letter };
+
+	// Collect all elements that appear only once
+	const singleElements = [];
+	for (const [key, value] of map.entries()) {
+		if (value === 1) {
+			singleElements.push(key);
+		}
+	}
+
+	// Return the array of single elements
+	return singleElements;
 }
-console.log(countNumberAndLetters('string152%¥'));
-console.log(countNumberAndLetters('JOSEPH770077%¥'));
+
+// Example usage
+console.log(singleNumber([4, 2, 1, 2, 1, 8, 9, 'a', 'k'])); // Should output: [4, 8, 9, 'a', 'k']
