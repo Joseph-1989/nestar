267f05614e3b5156303867f4d9700089f99000f0
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-08-20 20:21:55 +0900

    feat: deploy nestar monorepo project on docker containers

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger, Controller } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
 import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -6,31 +6,31 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
 	private readonly logger: Logger = new Logger();
+
 	public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
 		const recordTime = Date.now();
 		const requestType = context.getType<GqlContextType>();
 
 		if (requestType === 'http') {
-			// Develop if needed
+			/* Develop if needed! */
 		} else if (requestType === 'graphql') {
-			//  (1 ) Print request information
-
+			/* (1) Print Request */
 			const gqlContext = GqlExecutionContext.create(context);
-			this.logger.log(` ${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+			this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
 
-			// (2) => Errors handling via GraphQL
+			/* (2) Errors handling via GraphQL */
 
-			// (3) => No errors, giving Response below
+			/* (3) No Errors, giving Response below */
 			return next.handle().pipe(
 				tap((context) => {
 					const responseTime = Date.now() - recordTime;
-					this.logger.log(` ${this.stringify(context)} - ${responseTime}ms  \n\n`, 'response');
+					this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, 'RESPONSE');
 				}),
 			);
 		}
 	}
+
 	private stringify(context: ExecutionContext): string {
-		console.log(typeof context);
 		return JSON.stringify(context).slice(0, 75);
 	}
 }
